1431cc6132fe2cf1a4e1a6987aee9400b7decd52
Khazar Mammadli <xazar517@gmail.com>
2020-04-26 16:47:20 +0200

    Task3 Second issue fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -27,7 +27,7 @@ public class TorpedoStore {
       }
     }
   }
-
+  Random generator = new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
@@ -36,7 +36,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
