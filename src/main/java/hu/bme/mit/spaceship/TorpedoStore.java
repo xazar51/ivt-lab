e705de087a955dd91009c4b6f5ded68f1141f10b
Khazar Mammadli <xazar517@gmail.com>
2020-04-29 15:10:31 +0200

    Second issue fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -27,7 +27,7 @@ public class TorpedoStore {
       }
     }
   }
- 
+  Random generator = new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
